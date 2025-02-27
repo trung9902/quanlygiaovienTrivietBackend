510e248da50e5de5197ab09913b33204135a0e11
trung9902 <trung9a992002@gmail.com>
2025-02-28 03:10:28 +0700

    update28/2

--- TeacherManagementAPI/obj/Debug/net8.0/TeacherManagementAPI.AssemblyInfo.cs
+++ TeacherManagementAPI/obj/Debug/net8.0/TeacherManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeacherManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041fb21af13cc4cf5490e17961715bff418736c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeacherManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeacherManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
